a329440e342136a479e5abc5138b3f8f88615cf0
Raul de Heer <raul@supertask.nl>
2019-11-20 16:33:48 +0100

    ✨ Add controller properties

--- src/main/java/sagrada/controller/ChatController.java
+++ src/main/java/sagrada/controller/ChatController.java
@@ -1,4 +1,20 @@
 package sagrada.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
 public class ChatController {
+    @FXML
+    private TextField messageField;
+    @FXML
+    private Button sendButton;
+    @FXML
+    private VBox messageBox;
+
+    @FXML
+    public void initialize() {
+
+    }
 }
