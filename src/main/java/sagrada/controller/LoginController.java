b2410249461a547be17211345f3e8f80580abb38
Brian Soetens <brian40@live.nl>
2019-11-21 16:34:54 +0100

    ✨ Add login logic
    Not yet tested!

--- src/main/java/sagrada/controller/LoginController.java
+++ src/main/java/sagrada/controller/LoginController.java
@@ -1,11 +1,11 @@
 package sagrada.controller;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import sagrada.database.DatabaseConnection;
+
+import java.sql.SQLException;
 
 public class LoginController {
     @FXML
@@ -15,9 +15,33 @@ public class LoginController {
     private PasswordField pfPassword;
 
     @FXML
-    private Button submitButton;
+    protected void handleLogin() {
+        try {
+            var validLogin = this.doLogin();
 
-    @FXML
-    protected void handleLogin(ActionEvent event) {
+            if (validLogin) {
+                System.out.println("Logged in");
+            } else {
+                System.out.println("Invalid login");
+            }
+        } catch (SQLException e) {
+            System.out.println("Something went wrong!");
+        }
+    }
+
+    private boolean doLogin() throws SQLException {
+        var connection = new DatabaseConnection();
+
+        connection.connect();
+
+        var database = connection.getConnection();
+        var preparedStatement = database.prepareStatement("SELECT * FROM account WHERE username = ? AND password = ?");
+
+        preparedStatement.setString(1, this.tfUsername.getText());
+        preparedStatement.setString(2, this.pfPassword.getText());
+
+        var resultSet = preparedStatement.executeQuery();
+
+        return resultSet.getFetchSize() == 1;
     }
 }
