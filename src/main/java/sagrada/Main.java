6fb18b1721483b3498af5600488dd1c63db4bda2
Gilles Coolen <gilleswhc@gmail.com>
2019-11-21 15:38:21 +0100

    :lipstick: Actually replace styles with .css files

--- src/main/java/sagrada/Main.java
+++ src/main/java/sagrada/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        var sagrada = getClass().getResource("/views/lobby.fxml");
+        var sagrada = getClass().getResource("/views/login.fxml");
         Parent root = FXMLLoader.load(sagrada);
         primaryStage.setTitle("Sagrada");
         primaryStage.setScene(new Scene(root, 1200, 720));
