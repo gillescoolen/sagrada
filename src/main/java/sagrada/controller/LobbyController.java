30e3647834ce1691e3c6454f4c39760bccaa271a
Brian Soetens <brian40@live.nl>
2019-11-26 17:32:13 +0100

    💄 Fixed the lobbyItem to render in the list

--- src/main/java/sagrada/controller/LobbyController.java
+++ src/main/java/sagrada/controller/LobbyController.java
@@ -1,11 +1,22 @@
 package sagrada.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import sagrada.model.Account;
 
 public class LobbyController {
     private final Account user;
 
+    @FXML
+    private VBox vbLobbyItems;
+
     public LobbyController(Account account) {
         this.user = account;
+    }
+
+    @FXML
+    protected void initialize() {
+
     }
 }
