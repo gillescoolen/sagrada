b6cde798041dd3e064c4f0db13c13d00749d3587
Raul de Heer <raul@supertask.nl>
2019-12-03 14:17:48 +0100

    ✨ Add row color variety card

--- src/main/java/sagrada/model/card/objective/RowColorVariety.java
+++ src/main/java/sagrada/model/card/objective/RowColorVariety.java
@@ -3,6 +3,8 @@ package sagrada.model.card.objective;
 import sagrada.model.PatternCard;
 import sagrada.model.PublicObjectiveCard;
 
+import java.util.HashSet;
+
 public final class RowColorVariety extends PublicObjectiveCard {
     public RowColorVariety(int id, String name, String description, int points) {
         super(id, name, description, points);
@@ -10,6 +12,24 @@ public final class RowColorVariety extends PublicObjectiveCard {
 
     @Override
     public int calculatePoints(PatternCard patternCard) {
-        return 0;
+        var amountOfPoints = 0;
+
+        for (var yPosition = 1; yPosition < 4; ++yPosition) {
+            var rowColorSet = new HashSet<String>();
+
+            for (var xPosition = 1; xPosition < 5; ++xPosition) {
+                var square = patternCard.getSquareByXAndY(xPosition, yPosition);
+
+                if (square.getColor() != null) {
+                    rowColorSet.add(square.getColor().getColor());
+                }
+            }
+
+            if (rowColorSet.size() == 5) {
+                amountOfPoints += 6;
+            }
+        }
+
+        return amountOfPoints;
     }
 }
