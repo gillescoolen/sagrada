f29aa3fd251041fd12071aaef7b8ea833d927ecd
Haydar <haydarpehlivan61@hotmail.com>
2019-12-03 14:15:05 +0100

    ✨ add calculate sets function

--- src/main/java/sagrada/model/ObjectiveCard.java
+++ src/main/java/sagrada/model/ObjectiveCard.java
@@ -1,9 +1,18 @@
 package sagrada.model;
 
+import java.util.List;
+
 public abstract class ObjectiveCard extends Card {
     public ObjectiveCard(String name) {
         super(name);
     }
 
     public abstract int calculatePoints(PatternCard patternCard);
+
+    public int calculateSets(PatternCard patternCard, int first, int second) {
+        int firstNumber = (int) patternCard.getSquares().stream().filter(square -> square.getDie().getValue() == first).count();
+        int secondNumber = (int) patternCard.getSquares().stream().filter(square -> square.getDie().getValue() == second).count();
+
+        return Math.min(firstNumber, secondNumber);
+    }
 }
