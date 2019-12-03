d6e3121d13f6e741ad9c3afcf874684541be6e28
Haydar <haydarpehlivan61@hotmail.com>
2019-12-03 14:17:46 +0100

    ✨ add shade variety card

--- src/main/java/sagrada/model/card/objective/ShadeVariety.java
+++ src/main/java/sagrada/model/card/objective/ShadeVariety.java
@@ -3,6 +3,10 @@ package sagrada.model.card.objective;
 import sagrada.model.PatternCard;
 import sagrada.model.PublicObjectiveCard;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public final class ShadeVariety extends PublicObjectiveCard {
     public ShadeVariety(int id, String name, String description, int points) {
         super(id, name, description, points);
@@ -10,6 +14,13 @@ public final class ShadeVariety extends PublicObjectiveCard {
 
     @Override
     public int calculatePoints(PatternCard patternCard) {
-        return 0;
+        List<Integer> totals = new ArrayList<>();
+
+        for (int i = 0; i < 7; i++) {
+            int number = i;
+            totals.add((int) patternCard.getSquares().stream().filter(square -> square.getDie().getValue() == number).count());
+        }
+
+        return Collections.min(totals) * this.getPoints();
     }
 }
