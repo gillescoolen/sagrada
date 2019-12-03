6601434a8a456d7f1bc23db158858798c88e01c2
Haydar <haydarpehlivan61@hotmail.com>
2019-12-03 14:16:05 +0100

    ✨ add color variety card

--- src/main/java/sagrada/model/card/objective/ColorVariety.java
+++ src/main/java/sagrada/model/card/objective/ColorVariety.java
@@ -1,7 +1,12 @@
 package sagrada.model.card.objective;
 
+import sagrada.model.Color;
 import sagrada.model.PatternCard;
 import sagrada.model.PublicObjectiveCard;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public final class ColorVariety extends PublicObjectiveCard {
     public ColorVariety(int id, String name, String description, int points) {
@@ -10,7 +15,13 @@ public final class ColorVariety extends PublicObjectiveCard {
 
     @Override
     public int calculatePoints(PatternCard patternCard) {
-        return 0;
+        List<Integer> totals = new ArrayList<>();
+
+        for (Color color : Color.values()) {
+            totals.add((int) patternCard.getSquares().stream().filter(square -> square.getDie().getColor() == color).count());
+        }
+
+        return Collections.min(totals) * this.getPoints();
     }
 }
 
