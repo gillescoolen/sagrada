227f3c086a08cbf4779d5902c448dd99a52fe000
Raul de Heer <raul@supertask.nl>
2019-12-04 10:47:29 +0100

    👌 Change access modifier

--- src/main/java/sagrada/model/ObjectiveCard.java
+++ src/main/java/sagrada/model/ObjectiveCard.java
@@ -7,7 +7,7 @@ public abstract class ObjectiveCard extends Card {
 
     public abstract int calculatePoints(PatternCard patternCard);
 
-    public int calculateSets(PatternCard patternCard, int first, int second) {
+    protected int calculateSets(PatternCard patternCard, int first, int second) {
         int firstNumber = (int) patternCard.getSquares().stream().filter(square -> square.getDie().getValue() == first).count();
         int secondNumber = (int) patternCard.getSquares().stream().filter(square -> square.getDie().getValue() == second).count();
 
