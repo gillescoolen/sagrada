e9509923217cd51f23047133e2509d3fbf490515
Haydar <haydarpehlivan61@hotmail.com>
2019-12-05 16:39:40 +0100

    ✨ add get random tool card in repo

--- src/main/java/sagrada/database/repositories/ToolCardRepository.java
+++ src/main/java/sagrada/database/repositories/ToolCardRepository.java
@@ -3,26 +3,40 @@ package sagrada.database.repositories;
 import sagrada.database.DatabaseConnection;
 import sagrada.model.ToolCard;
 import sagrada.model.card.CardFactory;
-import sagrada.model.card.tool.*;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public final class ToolCardRepository extends Repository<ToolCard> {
     public ToolCardRepository(DatabaseConnection connection) {
         super(connection);
     }
 
+    public List<ToolCard> getRandom() throws SQLException {
+        PreparedStatement preparedStatement = this.connection.getConnection().prepareStatement("SELECT * FROM toolcard ORDER BY RAND() LIMIT 3;");
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        List<ToolCard> toolCards = new ArrayList<>();
+
+        while (resultSet.next()) {
+            toolCards.add(CardFactory.getToolCard(
+                    resultSet.getInt("idtoolcard"),
+                    resultSet.getString("name"),
+                    resultSet.getString("description")
+            ));
+        }
+
+        return toolCards;
+    }
+
     public ToolCard findByName(String name) throws SQLException {
         PreparedStatement preparedStatement = this.connection.getConnection().prepareStatement("SELECT * FROM toolcard WHERE `name` = ?");
         preparedStatement.setString(1, name);
         ResultSet resultSet = preparedStatement.executeQuery();
-
-        if (resultSet.getFetchSize() > 1) {
-            throw new SQLException("Multiple results, expected 1.");
-        }
 
         if (!resultSet.next()) {
             return null;
