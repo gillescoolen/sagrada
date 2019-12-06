884e5e1bf6ddf1aad37abb72aad1b0474bcf4438
Gilles Coolen <gilleswhc@gmail.com>
2019-12-06 16:51:24 +0100

    :bug: Close statements

--- src/main/java/sagrada/database/repositories/ChatRepository.java
+++ src/main/java/sagrada/database/repositories/ChatRepository.java
@@ -7,8 +7,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 public class ChatRepository extends Repository<ChatLine> {
     public ChatRepository(DatabaseConnection connection) {
@@ -16,10 +18,16 @@ public class ChatRepository extends Repository<ChatLine> {
     }
 
 
-    public void getMultiple(Date date) throws SQLException {
-        PreparedStatement preparedStatement = this.connection.getConnection().prepareStatement("");
+    public void getMultiple(LocalDateTime time, Integer id) throws SQLException {
+        PreparedStatement preparedStatement = this.connection.getConnection().prepareStatement("SELECT * FROM game JOIN player p on game.idgame = p.spel_idspel JOIN chatline c on p.idplayer = c.player_idplayer WHERE idgame = ? AND time > ?");
+
+        preparedStatement.setInt(1, id);
+        preparedStatement.setTimestamp(2, Timestamp.valueOf(time));
 
         ResultSet resultSet = preparedStatement.executeQuery();
+
+        preparedStatement.close();
+        resultSet.close();
     }
 
     @Override
@@ -56,11 +64,11 @@ public class ChatRepository extends Repository<ChatLine> {
         preparedStatement.setString(3, model.getMessage());
 
         preparedStatement.execute();
+        preparedStatement.close();
     }
 
     @Override
     public void addMultiple(Collection<ChatLine> models) throws SQLException {
 
     }
-
 }
