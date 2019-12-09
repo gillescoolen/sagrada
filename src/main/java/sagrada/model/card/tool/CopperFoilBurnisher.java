64bc2120ee532404894e7aacd6ce6f5054ce553f
Matthijs Kropholler <matthijsk2000@gmail.com>
2019-12-09 17:01:24 +0100

    ✨ implement CopperFoilBurnisher

--- src/main/java/sagrada/model/card/tool/CopperFoilBurnisher.java
+++ src/main/java/sagrada/model/card/tool/CopperFoilBurnisher.java
@@ -9,6 +9,13 @@ public final class CopperFoilBurnisher extends ToolCard {
 
     @Override
     public void use(DraftPool draftPool, DiceBag diceBag, PatternCard patternCard, RoundTrack roundTrack, Object message) {
+        Object[] values = (Object[]) message;
+        Square oldSquare = (Square)values[0];
+        Square newSquare = (Square)values[1];
+
+        newSquare.setDie(oldSquare.getDie());
+        oldSquare.setDie(null);
+
         this.incrementCost();
     }
 }
