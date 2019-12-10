5ad676dbfef07b7166c3ea818144fdc54b4f4f45
Haydar <haydarpehlivan61@hotmail.com>
2019-12-10 19:55:03 +0100

    ✨ add glazing hammer logic

--- src/main/java/sagrada/model/card/tool/GlazingHammer.java
+++ src/main/java/sagrada/model/card/tool/GlazingHammer.java
@@ -1,16 +1,26 @@
 package sagrada.model.card.tool;
 
 import sagrada.database.DatabaseConnection;
+import sagrada.database.repositories.FavorTokenRepository;
 import sagrada.model.*;
 
+import java.sql.SQLException;
+
 public final class GlazingHammer extends ToolCard {
+    private FavorTokenRepository favorTokenRepository = new FavorTokenRepository(this.connection);
+
     public GlazingHammer(int id, String name, String description, DatabaseConnection connection) {
         super(id, name, description, connection);
     }
 
     @Override
-    public void use(DraftPool draftPool, DiceBag diceBag, PatternCard patternCard, RoundTrack roundTrack, Object message) {
+    public void use(DraftPool draftPool, DiceBag diceBag, PatternCard patternCard, RoundTrack roundTrack, Player player, Game game, Object message) throws SQLException {
         draftPool.throwDice();
         this.incrementCost();
+
+        FavorToken favorToken = player.getNonAffectedFavorToken();
+        favorToken.setToolCard(this);
+
+        favorTokenRepository.updateFavorToken(favorToken, this.getId(), roundTrack.getCurrent(), false);
     }
 }
