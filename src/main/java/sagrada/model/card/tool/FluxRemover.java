90732311bbbe827501dffb48c2c8263949cb78b9
Matthijs Kropholler <matthijsk2000@gmail.com>
2019-12-10 20:48:30 +0100

    :sparkles: add flux remover

--- src/main/java/sagrada/model/card/tool/FluxRemover.java
+++ src/main/java/sagrada/model/card/tool/FluxRemover.java
@@ -3,13 +3,28 @@ package sagrada.model.card.tool;
 import sagrada.database.DatabaseConnection;
 import sagrada.model.*;
 
+import java.sql.SQLException;
+
 public final class FluxRemover extends ToolCard {
     public FluxRemover(int id, String name, String description, DatabaseConnection connection) {
         super(id, name, description, connection);
     }
 
     @Override
-    public void use(DraftPool draftPool, DiceBag diceBag, PatternCard patternCard, RoundTrack roundTrack, Object message) {
+    public void use(DraftPool draftPool, DiceBag diceBag, PatternCard patternCard, RoundTrack roundTrack, Player player, Game game, Object message) throws SQLException {
+        Object[] messages = (Object[]) message;
+        Die die = (Die) messages[0];
+        int newValue = (Integer) messages[1];
+
+        diceBag.put(die);
+        Die newDie = diceBag.getRandomDice(1).get(0);
+        newDie.setValue(newValue);
+        
+        draftPool.updateDraft(die, newDie);
+
         this.incrementCost();
+
+        // TODO: implement database garbage kanker zooi ik wil fucking dood ajfdoiasjdfojajoasjdfojaf allahuakbar
+        // inshallah mijn broer haydar
     }
 }
