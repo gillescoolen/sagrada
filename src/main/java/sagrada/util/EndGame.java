6f6cc773208b8b88738728cfe053bf7cdb626ddd
Antal Visser <antalvisser@gmail.com>
2019-12-12 21:04:59 +0100

    🚧 Add tie breaking

--- src/main/java/sagrada/util/EndGame.java
+++ src/main/java/sagrada/util/EndGame.java
@@ -17,15 +17,13 @@ public class EndGame {
         this.connection = connection;
     }
 
-    public void correctAllPatternCards() {
+    public void calculatePoints() {
         var updatedPlayerList = new ArrayList<Player>();
-        var objectivePoints = new ArrayList<Integer>();
-        var favorTokens = new ArrayList<Integer>();
-        Player playerWithMostObjectivePoints;
-        Player playerWithMostFavorTokens;
+        var tiePlayerList = new ArrayList<Player>();
 
         for (var player : this.game.getPlayers()) {
             var points = 0;
+
             player.setPlayerFrame(player.validateFrameField(this.connection, this.game));
 
             for (var objectiveCard : this.game.getObjectiveCards()) {
@@ -37,27 +35,92 @@ public class EndGame {
 
             points += privateObjectiveCardPoints;
             points += remainingFavorTokens;
-            points += player.getPlayerFrame().countEmptySquares();
-
-            objectivePoints.add(privateObjectiveCardPoints);
-            favorTokens.add(remainingFavorTokens);
-
-            for (var objectivePoint : objectivePoints) {
-                if (objectivePoint ) {
-
-                }
-            }
-
-            for (var favorToken : favorTokens ) {
-
-            }
+            points -= player.getPlayerFrame().countEmptySquares();
 
             player.setScore(points);
             updatedPlayerList.add(player);
         }
 
+        Player bestScoringPlayer = null;
+        var highestScore = 0;
+        for (var player : updatedPlayerList) {
+            if (bestScoringPlayer == null) {
+                bestScoringPlayer = player;
+                highestScore = player.getScore();
+            } else {
+                if (highestScore == player.getScore()) {
+                    if (!tiePlayerList.contains(bestScoringPlayer)) {
+                        tiePlayerList.add(bestScoringPlayer);
+                    }
+                    tiePlayerList.add(player);
+                } else if (highestScore < player.getScore()) {
+                    bestScoringPlayer = player;
+                    highestScore = player.getScore();
+                }
+            }
+        }
 
+        if (!tiePlayerList.isEmpty()) {
+            var winner = getWinnerWhenTie(tiePlayerList);
+            // TODO: add 1 point for the winner
+        }
 
         this.game.addPlayers(updatedPlayerList);
+    }
+
+    private Player getWinnerWhenTie(ArrayList<Player> tiePlayerList) {
+        var tiePrivatePoints = new ArrayList<Player>();
+        Player bestScoringPlayer = null;
+        var highestScore = 0;
+        for (var player : tiePlayerList) {
+            var points = player.getPrivateObjectiveCard().calculatePoints(player.getPlayerFrame());
+
+            if (bestScoringPlayer == null) {
+                bestScoringPlayer = player;
+                highestScore = points;
+            } else {
+                if (highestScore == points) {
+                    if (!tiePrivatePoints.contains(bestScoringPlayer)) {
+                        tiePrivatePoints.add(bestScoringPlayer);
+                    }
+                    tiePrivatePoints.add(player);
+                } else if (highestScore < points) {
+                    bestScoringPlayer = player;
+                    highestScore = points;
+                }
+            }
+        }
+
+        if (tiePrivatePoints.isEmpty()) {
+            return bestScoringPlayer;
+        }
+
+        var tieFavorPoints = new ArrayList<Player>();
+        bestScoringPlayer = null;
+        highestScore = 0;
+        for (var player : tiePlayerList) {
+            var points = player.getFavorTokens().size();
+
+            if (bestScoringPlayer == null) {
+                bestScoringPlayer = player;
+                highestScore = points;
+            } else {
+                if (highestScore == points) {
+                    if (!tieFavorPoints.contains(bestScoringPlayer)) {
+                        tieFavorPoints.add(bestScoringPlayer);
+                    }
+                    tieFavorPoints.add(player);
+                } else if (highestScore < points) {
+                    bestScoringPlayer = player;
+                    highestScore = points;
+                }
+            }
+        }
+
+        if (tieFavorPoints.isEmpty()) {
+            return bestScoringPlayer;
+        }
+
+        // TODO: decide winner by reverse player order
     }
 }
