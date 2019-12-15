dba8ee9f67b79e4a82dc44eeacc88cc564530b3a
Haydar <haydarpehlivan61@hotmail.com>
2019-12-16 00:24:12 +0100

    🐛 fix calculation of column variety

--- src/main/java/sagrada/model/card/objective/ColumnShadeVariety.java
+++ src/main/java/sagrada/model/card/objective/ColumnShadeVariety.java
@@ -14,10 +14,10 @@ public final class ColumnShadeVariety extends PublicObjectiveCard {
     public int calculatePoints(PatternCard patternCard) {
         var amountOfPoints = 0;
 
-        for (var xPosition = 1; xPosition < 5; ++xPosition) {
+        for (var xPosition = 1; xPosition <= 5; ++xPosition) {
             var columnValueSet = new HashSet<Integer>();
 
-            for (var yPosition = 1; yPosition < 4; ++yPosition) {
+            for (var yPosition = 1; yPosition <= 4; ++yPosition) {
                 var square = patternCard.getSquareByXAndY(xPosition, yPosition);
 
                 if (square.getDie() != null) {
@@ -26,7 +26,7 @@ public final class ColumnShadeVariety extends PublicObjectiveCard {
             }
 
             if (columnValueSet.size() == 4) {
-                amountOfPoints += 4;
+                amountOfPoints += this.getPoints();
             }
         }
 
