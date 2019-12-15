a01081f6f62e8a363c4506b5118dd2237ca3eda1
Antal Visser <antalvisser@gmail.com>
2019-12-15 16:35:58 +0100

    ✨  Finish FluxRemover

--- src/main/java/sagrada/model/card/tool/FluxRemover.java
+++ src/main/java/sagrada/model/card/tool/FluxRemover.java
@@ -1,7 +1,9 @@
 package sagrada.model.card.tool;
 
 import sagrada.database.DatabaseConnection;
+import sagrada.database.repositories.DieRepository;
 import sagrada.database.repositories.FavorTokenRepository;
+import sagrada.database.repositories.GameRepository;
 import sagrada.database.repositories.ToolCardRepository;
 import sagrada.model.*;
 
@@ -11,6 +13,8 @@ import java.util.ArrayList;
 public final class FluxRemover extends ToolCard {
     private ToolCardRepository toolCardRepository = new ToolCardRepository(this.connection);
     private FavorTokenRepository favorTokenRepository = new FavorTokenRepository(this.connection);
+    private GameRepository gameRepository = new GameRepository(this.connection);
+    private DieRepository dieRepository = new DieRepository(this.connection);
 
     public FluxRemover(int id, String name, String description, DatabaseConnection connection) {
         super(id, name, description, connection);
@@ -20,9 +24,11 @@ public final class FluxRemover extends ToolCard {
     public void use(DraftPool draftPool, DiceBag diceBag, PatternCard patternCard, RoundTrack roundTrack, Player player, Game game, Object message) throws SQLException {
         Object[] messages = (Object[]) message;
         Die die = (Die) messages[0];
-        int newValue = (Integer) messages[1];
+        Integer newValue = (Integer) messages[1];
 
+        dieRepository.removeGameDie(game.getId(), die);
         diceBag.put(die);
+
         Die newDie = diceBag.getRandomDice(1).get(0);
         newDie.setValue(newValue);
 
@@ -33,10 +39,12 @@ public final class FluxRemover extends ToolCard {
         ArrayList<Die> dice = new ArrayList<>();
         dice.add(newDie);
 
+        var round = gameRepository.getCurrentRound(game.getId());
+        dieRepository.addGameDice(game.getId(), round, dice);
+
         FavorToken favorToken = player.getNonAffectedFavorToken();
         favorToken.setToolCard(this);
 
         favorTokenRepository.updateFavorToken(favorToken, this.getId(), roundTrack.getCurrent(), false);
-        toolCardRepository.addAffectedToolCard(this, dice, game.getId());
     }
 }
